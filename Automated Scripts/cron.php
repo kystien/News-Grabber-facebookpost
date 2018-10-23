7c2e3b184d01bfcf7eb5bfd3d2f0285af898a3fe
Jesse Poland <jpoland25@gmail.com>
2018-10-23 13:23:49 -0600

    Update cron.php

--- Automated Scripts/cron.php
+++ Automated Scripts/cron.php
@@ -1,16 +1,17 @@
 #!/usr/bin/php
 <?php
-	require_once ('/home/admin/Config/config.php');
-	require ('/home/admin/updater/update.php');
-	require ('/home/admin/updater/twpull.php');
-	unlink('/home/admin/json/rows.json');
-	shell_exec('php /home/admin/updater/rows.php');
-	unlink('/home/admin/json/tquery.json');
-	shell_exec('php /home/admin/updater/tquery.php');
-	unlink('/home/admin/json/query.json');
-	shell_exec('php /home/admin/updater/query.php');
-	unlink('/home/admin/json/tlxquery.json');
-	shell_exec('php /home/admin/updater/merge.php');
-	shell_exec('php /home/admin/updater/telegram.php');
+	require_once ('/src/config.php');
+	require ('update.php');
+	require ('twpull.php');
+	unlink('/json/rows.json');
+	shell_exec('php rows.php');
+	unlink('/json/tquery.json');
+	shell_exec('php tquery.php');
+	unlink('json/query.json');
+	shell_exec('php query.php');
+	unlink('json/tlxquery.json');
+	shell_exec('php merge.php');
+	shell_exec('php telegram.php');
 	exit;
 ?>
+Run cron with: */5 * * * * /usr/bin/php path/to/automated/scripts/cron.php >/dev/null
